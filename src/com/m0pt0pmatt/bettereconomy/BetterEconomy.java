69af0afd223a21abcbc234f0349eb2ccf9f8d444
Matthew Broomfield <m0pt0pmatt17@gmail.com>
2013-03-08 21:16:33 -0700

    Started Project for reals

---
+++ src/com/m0pt0pmatt/bettereconomy/BetterEconomy.java
@@ -0,0 +1,10 @@
+package com.m0pt0pmatt.bettereconomy;
+
+
+public class BetterEconomy {
+
+	
+	public static EconomyManager economy = null;
+	public static FileManager fileManager = null;
+	
+}
---
+++ src/com/m0pt0pmatt/bettereconomy/InventoryAccount.java
@@ -0,0 +1,18 @@
+package com.m0pt0pmatt.bettereconomy;
+
+public class InventoryAccount extends Account{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InventoryAccount(String owner){
+		super(owner);
+	}
+	
+	public InventoryAccount(String owner, double balance){
+		super(owner, balance);
+	}
+
+}
